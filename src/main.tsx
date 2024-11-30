a43ff862801fbf89a9bb965335cba0de0078674f
Julian <julianamadors1@hotmail.com>
2024-11-29 23:51:10 -0500

    Descripción de los cambios realizados

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './components/About.tsx';
+import App from './App.tsx';
+import './index.css';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 
@@ -11,7 +13,12 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<About />} />
+        <Route path="/app" element={<App />} />
       </Routes>
     </Router>
   </React.StrictMode>
 );
+
+
+
+

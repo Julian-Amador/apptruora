718d1c6cd8fcb3c68ad3ef33bb0e7f7e1e2ef510
Julian <julianamadors1@hotmail.com>
2024-11-29 23:44:49 -0500

    Descripción de los cambios realizados

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Julian-Amador.github.io/apptruora/',
+  base: '/apptruora/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
